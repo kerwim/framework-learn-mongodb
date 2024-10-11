8deac3a91e8f6ae9ef9a21deab4943086024e0c4
mac <0924>
2024-10-11 23:31:41 +0800

    intro

---
+++ src/main/java/com/kerwim/frameworklearnmongodb/FrameworkLearnMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearnMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkLearnMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerwim/frameworklearnmongodb/FrameworkLearnMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkLearnMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
